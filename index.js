de7410b66ca154feba91eb67f310187fa529799f
eppakayalaNaresh <eppakayalanaresh08@gmail.com>
2023-01-01 11:43:39 +0000

    Super Over League tests

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bgContainer">
+    <h1 className="headingSuperOver">Super Over League</h1>
+    <div className="img-Container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
